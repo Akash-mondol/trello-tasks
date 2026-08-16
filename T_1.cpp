e63a99f6c1dd730f961a48235e68181b62c7026d
mohammad-akash <akasham355@gmail.com>
2026-08-16 23:40:00 +0600

    fix: increase address capacity

--- T_1.cpp
+++ T_1.cpp
@@ -19,7 +19,7 @@ int main()
     char department[50];
     char email[100];
     char phone[20];
-    char address[100];
+    char address[200];
     char gender[10];
     int semester;
     int creditHours;
